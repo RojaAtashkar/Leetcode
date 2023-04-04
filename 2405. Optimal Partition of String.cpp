fd17c339449b6ff25f7de6826d33f07dcd3bc63a
RojaAtashkar <103642013+RojaAtashkar@users.noreply.github.com>
2023-04-04 14:05:19 +0330

    Update 2405. Optimal Partition of String.cpp

--- 2405. Optimal Partition of String.cpp
+++ 2405. Optimal Partition of String.cpp
@@ -15,3 +15,21 @@ public:
      return count;
     }
 };
+
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<int> lastSeen(26, -1);
+        int count = 1, seen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (lastSeen[s[i] - 'a'] >= seen) {
+                count++;
+                seen = i;
+            }
+            lastSeen[s[i] - 'a'] = i;
+        }
+
+        return count;
+    }
+};
